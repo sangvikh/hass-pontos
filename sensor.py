91832c5efc82aac597ee44fd44eecaac862d5402
Harald Sangvik <harald.sangvik@gmail.com>
2024-04-23 02:43:14 +0200

    All sensors sharing a single http session, values fetching properly now

--- sensor.py
+++ sensor.py
@@ -1,7 +1,6 @@
 import asyncio
 from datetime import datetime, timedelta
 import aiohttp
-
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import VOLUME_LITERS
 from . import DOMAIN
@@ -11,8 +10,10 @@ FETCH_INTERVAL = timedelta(seconds=60)  # Set fetch interval to 60 seconds
 class PontosSensor(SensorEntity):
     _last_fetch = None
     _data = None
+    _lock = asyncio.Lock()
 
-    def __init__(self, ip, name, endpoint, unit, device_class):
+    def __init__(self, session, ip, name, endpoint, unit, device_class):
+        self._session = session
         self._ip = ip
         self._attr_name = name
         self._endpoint = endpoint
@@ -20,39 +21,39 @@ class PontosSensor(SensorEntity):
         self._attr_device_class = device_class
 
     async def async_update(self):
-        if not self._data or not self._last_fetch or (datetime.now() - self._last_fetch) > FETCH_INTERVAL:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"http://{self._ip}:5333/pontos-base/get/all") as response:
+        async with PontosSensor._lock:
+            if not PontosSensor._data or not PontosSensor._last_fetch or (datetime.now() - PontosSensor._last_fetch) > FETCH_INTERVAL:
+                async with self._session.get(f"http://{self._ip}:5333/pontos-base/get/all") as response:
                     if response.status == 200:
                         PontosSensor._data = await response.json()
                         PontosSensor._last_fetch = datetime.now()
                     else:
-                        raise Exception(f"Failed to fetch data: HTTP {response.status}")
-    
+                        _LOGGER.error(f"Failed to fetch data: HTTP {response.status}")
+
     @property
     def state(self):
-        return self._data.get(self._endpoint) if self._data else None
+        return PontosSensor._data.get(self._endpoint) if PontosSensor._data else None
 
 async def async_setup_entry(hass, entry, async_add_entities):
     config = entry.data
     ip_address = config['ip_address']
+    session = aiohttp.ClientSession()
     async_add_entities([
-        PontosSensor(ip_address, "Total consumption in liters", "getVOL", VOLUME_LITERS, "water"),
-        PontosSensor(ip_address, "Water pressure", "getBAR", "mbar", "pressure"),
-        PontosSensor(ip_address, "Water temperature", "getCEL", "°C", "temperature"),
-        PontosSensor(ip_address, "Time in seconds since turbine received no pulse", "getNPS", "s", None),
-        PontosSensor(ip_address, "Volume of current water consumption in ml", "getAVO", "mL", "water"),
-        PontosSensor(ip_address, "Configured Micro Leakage Test, pressure drop in bar", "getDBD", "bar", "pressure"),
-        PontosSensor(ip_address, "Wifi connection state", "getWFS", None, None),
-        PontosSensor(ip_address, "Wifi signal strength (RSSI)", "getWFR", "dB", "signal_strength"),
-        PontosSensor(ip_address, "Battery voltage", "getBAT", "V", "voltage"),
-        PontosSensor(ip_address, "Mains voltage", "getNET", "V", "voltage"),
-        PontosSensor(ip_address, "Serial number", "getSRN", None, None),
-        PontosSensor(ip_address, "Firmware version", "getVER", None, None),
-        PontosSensor(ip_address, "Type", "getTYP", None, None),
-        PontosSensor(ip_address, "Code number", "getCNO", None, None),
-        PontosSensor(ip_address, "MAC Address", "getMAC", None, None),
-        PontosSensor(ip_address, "Alarm", "getALA", None, None),
-        PontosSensor(ip_address, "Active profile", "getPRF", None, None),
-        PontosSensor(ip_address, "Valve status", "getVLV", None, None)
+        PontosSensor(session, ip_address, "Total consumption in liters", "getVOL", VOLUME_LITERS, "water"),
+        PontosSensor(session, ip_address, "Water pressure", "getBAR", "mbar", "pressure"),
+        PontosSensor(session, ip_address, "Water temperature", "getCEL", "°C", "temperature"),
+        PontosSensor(session, ip_address, "Time in seconds since turbine received no pulse", "getNPS", "s", None),
+        PontosSensor(session, ip_address, "Volume of current water consumption in ml", "getAVO", "mL", "water"),
+        PontosSensor(session, ip_address, "Configured Micro Leakage Test, pressure drop in bar", "getDBD", "bar", "pressure"),
+        PontosSensor(session, ip_address, "Wifi connection state", "getWFS", None, None),
+        PontosSensor(session, ip_address, "Wifi signal strength (RSSI)", "getWFR", "dB", "signal_strength"),
+        PontosSensor(session, ip_address, "Battery voltage", "getBAT", "V", "voltage"),
+        PontosSensor(session, ip_address, "Mains voltage", "getNET", "V", "voltage"),
+        PontosSensor(session, ip_address, "Serial number", "getSRN", None, None),
+        PontosSensor(session, ip_address, "Firmware version", "getVER", None, None),
+        PontosSensor(session, ip_address, "Type", "getTYP", None, None),
+        PontosSensor(session, ip_address, "MAC Address", "getMAC", None, None),
+        PontosSensor(session, ip_address, "Alarm", "getALA", None, None),
+        PontosSensor(session, ip_address, "Active profile", "getPRF", None, None),
+        PontosSensor(session, ip_address, "Valve status", "getVLV", None, None)
     ])
