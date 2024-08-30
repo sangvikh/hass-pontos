c178713f4f1fc663cb40fdc55478043a5908148c
Harald Sangvik <harald.sangvik@gmail.com>
2024-08-30 02:52:15 +0200

    Moved parsing back to sensor.py
    Parsing is depenent on the PontosSensor class, so not really usable elsewhere

--- custom_components/hass_pontos/sensor.py
+++ custom_components/hass_pontos/sensor.py
@@ -2,7 +2,7 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.event import async_track_time_interval
 import logging
 
-from .utils import fetch_data, parse_data
+from .utils import fetch_data
 from .device import get_device_info
 from .const import CONF_IP_ADDRESS, SENSOR_DETAILS, FETCH_INTERVAL, URL_LIST
 
@@ -18,20 +18,20 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     # Update data so sensors is available immediately
     for sensor in sensors:
-        sensor.set_data(parse_data(data, sensor))
+        sensor.set_state(sensor.parse_data(data))
 
-    # Function to fetch new data and update all sensors
-    async def update_data(_):
-        data = await fetch_data(ip_address, URL_LIST)
+    # Function to fetch new state and update all sensors
+    async def update_state(_):
+        state = await fetch_data(ip_address, URL_LIST)
         for sensor in sensors:
-                sensor.set_data(parse_data(data, sensor))
+                sensor.set_state(sensor.parse_data(data))
 
     # Schedule updates using the fetch interval
-    async_track_time_interval(hass, update_data, FETCH_INTERVAL)
+    async_track_time_interval(hass, update_state, FETCH_INTERVAL)
 
 class PontosSensor(SensorEntity):
     def __init__(self, sensor_config, device_info):
-        self._data = None
+        self._state = None
         self._attr_name = f"{device_info['name']} {sensor_config['name']}"
         self._endpoint = sensor_config['endpoint']
         self._attr_native_unit_of_measurement = sensor_config.get('unit', None)
@@ -43,8 +43,8 @@ class PontosSensor(SensorEntity):
         self._attr_unique_id = f"{device_info['serial_number']}_{sensor_config['name']}"
         self._device_info = device_info
 
-    def set_data(self, data):
-        self._data = data
+    def set_state(self, state):
+        self._state = state
         self.async_write_ha_state()
 
     @property
@@ -59,4 +59,29 @@ class PontosSensor(SensorEntity):
 
     @property   
     def state(self):
-        return self._data
+        return self._state
+
+    # Parsing sensor data
+    def parse_data(self, data):
+        """Process, format, and validate sensor data."""
+        if data is None:
+            return None
+        _data = data.get(self._endpoint, None)
+
+        # Apply format replacements if format_dict is present
+        if self._format_dict is not None and _data is not None:
+            for old, new in self._format_dict.items():
+                _data = _data.replace(old, new)
+
+        # Translate alarm codes if code_dict is present
+        if self._code_dict is not None and _data is not None:
+            _data = self._code_dict.get(_data, _data)
+
+        # Scale sensor data if scale is present
+        if self._scale is not None and _data is not None:
+            try:
+                _data = float(_data) * self._scale
+            except (ValueError, TypeError):
+                pass
+
+        return _data
