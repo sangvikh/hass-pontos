9232d011838e5b8c6bae9a94dd7d584ff5e5fee9
Harald Sangvik <harald.sangvik@gmail.com>
2024-08-30 04:13:42 +0200

    Add event listener to valve entity

--- custom_components/hass_pontos/valve.py
+++ custom_components/hass_pontos/valve.py
@@ -1,23 +1,28 @@
 import logging
 from homeassistant.components.valve import ValveEntity, ValveEntityFeature, ValveDeviceClass
-from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.helpers.event import async_track_state_change_event
+from homeassistant.core import callback, Event
 from .device import get_device_info
-from .const import DOMAIN, VALVE_STATUS_SENSOR, FETCH_INTERVAL
+from .const import DOMAIN, VALVE_STATUS_SENSOR
 
 LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the valve entity."""
-    device_info, _ = await get_device_info(hass, entry)
+    device_info, data = await get_device_info(hass, entry)
 
-    # Explicitly instantiate the PontosValve entity
+    # Fetch initial state from the sensor entity
+    sensor_state = hass.states.get(VALVE_STATUS_SENSOR)
+    initial_state = sensor_state.state if sensor_state else None
+
+    # Instantiate the PontosValve entity
     valve_entity = PontosValve(hass, entry, device_info)
 
     # Add the entity to Home Assistant
     async_add_entities([valve_entity], True)
 
-    # Schedule periodic updates to check the valve state
-    async_track_time_interval(hass, valve_entity.async_update, FETCH_INTERVAL)
+    # Set initial state
+    valve_entity.set_state(initial_state)
 
 class PontosValve(ValveEntity):
     """Representation of the Pontos Valve entity."""
@@ -30,8 +35,27 @@ class PontosValve(ValveEntity):
         self._attr_unique_id = f"{device_info['serial_number']}_water_supply"
         self._attr_reports_position = False
         self._attr_device_class = ValveDeviceClass.WATER
-        self._state = None  # Initial state will be set during the first update
+        self._state = None
         self._device_info = device_info
+
+    def set_state(self, state):
+        self._state = state
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self):
+        """When entity is added to hass."""
+        await super().async_added_to_hass()
+        async_track_state_change_event(
+            self.hass,
+            VALVE_STATUS_SENSOR,
+            self._sensor_state_changed
+        )
+
+    @callback
+    def _sensor_state_changed(self, event: Event) -> None:
+        new_state = event.data.get('new_state')
+        if new_state is not None:
+            self.set_state(new_state.state)
 
     @property   
     def state(self):
@@ -80,20 +104,3 @@ class PontosValve(ValveEntity):
             "close_valve",
             service_data={"entry_id": self._entry.entry_id}
         )
-
-    async def async_update(self, time_event=None):
-        """Update the valve's state from the valve status sensor."""
-        sensor_entity_id = f"{VALVE_STATUS_SENSOR}"
-
-        # Fetch the state of the sensor
-        sensor_state = self._hass.states.get(sensor_entity_id)
-
-        if sensor_state:
-            self._state = sensor_state.state
-        else:
-            LOGGER.error(f"Could not find sensor state for {sensor_entity_id}")
-            self._state = None  # If sensor state is not found, set to None
-
-        # Write the state to Home Assistant
-        if self.entity_id:
-            self.async_write_ha_state()
