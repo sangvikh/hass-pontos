65ca80be5b78da1f4a1b506f03e5f58633c2a318
zlFamous <>
2025-09-18 13:45:41 +0200

    changing endpoints

--- custom_components/hass_pontos/device_config/conf_neosoft.py
+++ custom_components/hass_pontos/device_config/conf_neosoft.py
@@ -13,32 +13,10 @@ URL_LIST = [
     URL_ALL_DATA
 ]
 
-SALT_STATUS_CODES = {
-    "0": "salt_level_ok",
-    "1": "salt_level_low",
-    "2": "salt_refill_required"
-}
-
 REGEN_STATE_CODES = {
     "0": "regeneration_idle",
-    "1": "regeneration_pending",
-    "2": "regeneration_running",
-    "3": "regeneration_rinsing",
-    "4": "regeneration_error"
-}
-
-REGEN_TRIGGER_CODES = {
-    "0": "trigger_none",
-    "1": "trigger_volume",
-    "2": "trigger_time",
-    "3": "trigger_manual",
-    "4": "trigger_service"
-}
-
-OPERATING_MODE_CODES = {
-    "0": "mode_service",
-    "1": "mode_bypass",
-    "2": "mode_standby"
+    "1": "regeneration_bottle_1",
+    "2": "regeneration_bottle_2",
 }
 
 SENSOR_DETAILS = {
@@ -51,74 +29,45 @@ SENSOR_DETAILS = {
     },
     "raw_water_hardness": {
         "name": "Raw water hardness",
-        "endpoint": "getHWI",
-        "unit": "°dH",
-        "scale": 0.1
-    },
-    "treated_water_hardness": {
-        "name": "Treated water hardness",
-        "endpoint": "getHWO",
+        "endpoint": "getIWH",
         "unit": "°dH",
         "scale": 0.1
     },
     "target_water_hardness": {
         "name": "Target water hardness",
-        "endpoint": "getHTG",
+        "endpoint": "getOWH",
         "unit": "°dH",
         "scale": 0.1,
         "entity_category": EntityCategory.CONFIG
     },
     "salt_level": {
-        "name": "Salt level",
-        "endpoint": "getSLP",
-        "unit": "%",
+        "name": "Salt amount",
+        "endpoint": "getSV1",
+        "unit": "kg",
         "entity_category": EntityCategory.DIAGNOSTIC
     },
-    "salt_alarm": {
-        "name": "Salt alarm",
-        "endpoint": "getSLS",
-        "code_dict": SALT_STATUS_CODES,
+    "salt_level": {
+        "name": "Salt supply",
+        "endpoint": "getSS1",
+        "unit": "weeks",
         "entity_category": EntityCategory.DIAGNOSTIC
-    },
-    "operating_mode": {
-        "name": "Operating mode",
-        "endpoint": "getOPS",
-        "code_dict": OPERATING_MODE_CODES,
-        "entity_category": EntityCategory.CONFIG
     },
     "regeneration_state": {
         "name": "Regeneration state",
-        "endpoint": "getRGN",
+        "endpoint": "getRG1",
         "code_dict": REGEN_STATE_CODES,
-        "entity_category": EntityCategory.DIAGNOSTIC
-    },
-    "regeneration_trigger": {
-        "name": "Last regeneration trigger",
-        "endpoint": "getRGT",
-        "code_dict": REGEN_TRIGGER_CODES,
         "entity_category": EntityCategory.DIAGNOSTIC
     },
     "regeneration_time_remaining": {
         "name": "Regeneration time remaining",
-        "endpoint": "getRGR",
+        "endpoint": "getRTI",
         "unit": "min",
         "entity_category": EntityCategory.DIAGNOSTIC
     },
     "regeneration_volume_remaining": {
         "name": "Water until regeneration",
-        "endpoint": "getRGV",
+        "endpoint": "getRE1",
         "unit": "L",
-        "entity_category": EntityCategory.DIAGNOSTIC
-    },
-    "days_since_regeneration": {
-        "name": "Days since last regeneration",
-        "endpoint": "getRGD",
-        "unit": "d",
-        "entity_category": EntityCategory.DIAGNOSTIC
-    },
-    "next_regeneration_time": {
-        "name": "Next regeneration time",
-        "endpoint": "getRGTm",
         "entity_category": EntityCategory.DIAGNOSTIC
     },
     "wifi_state": {
@@ -156,22 +105,6 @@ SENSOR_DETAILS = {
 }
 
 SERVICES = {
-    "start_regeneration": {
-        "name": "Start regeneration",
-        "endpoint": "set/rgn/start"
-    },
-    "stop_regeneration": {
-        "name": "Stop regeneration",
-        "endpoint": "set/rgn/stop"
-    },
-    "set_target_hardness": {
-        "name": "Set target water hardness",
-        "endpoint": "set/htg/{hardness}"
-    },
-    "set_operating_mode": {
-        "name": "Set operating mode",
-        "endpoint": "set/ops/{mode}"
-    },
     "generic_service": {
         "name": "Generic service call",
         "endpoint": "set/{endpoint}/{data}"
