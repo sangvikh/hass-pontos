5cdfdbfa1d55e95782de8eca5bf813514c8d8d47
Harald Sangvik <harald.sangvik@nov.com>
2025-09-22 10:04:07 +0200

    Neosoft: Remove services not present, added alarm code config.

--- custom_components/hass_pontos/device_config/conf_neosoft.py
+++ custom_components/hass_pontos/device_config/conf_neosoft.py
@@ -13,10 +13,51 @@ URL_LIST = [
     URL_ALL_DATA
 ]
 
+ALARM_CODES = {
+    "FF": "no_alarm",
+    "A1": "alarm_end_switch",
+    "A2": "alarm_turbine_blocked",
+    "A3": "alarm_leakage_volume_reached",
+    "A4": "alarm_leakage_time_reached",
+    "A5": "alarm_max_flow_rate_reached",
+    "A6": "alarm_microleakage_detected",
+    "A7": "alarm_external_sensor_leakage_radio",
+    "A8": "alarm_external_sensor_leakage_cable",
+    "A9": "alarm_pressure_sensor_faulty",
+    "AA": "alarm_temperature_sensor_faulty",
+    "AB": "fault_conductance_sensor",
+    "AC": "fault_conductance_sensor",
+    "AE": "error_no_information",
+    "AD": "alarm_increased_water_hardness",
+    "0D": "alarm_salt_supply_empty",
+    "0E": "alarm_valve_position",
+}
+
+NOTIFICATION_CODES = {
+    "01": "new_software_available",
+    "04": "new_software_installed",
+    "FF": "no_notification"
+}
+
+WARNING_CODES = {
+    "01": "power_outage",
+    "07": "leak_warning",
+    "08": "battery_low",
+    "02": "salt_supply_low",
+    "FF": "no_warning"
+}
+
 REGEN_STATE_CODES = {
     "0": "regeneration_idle",
     "1": "regeneration_bottle_1",
     "2": "regeneration_bottle_2",
+}
+
+REGEN_MODE_CODES = {
+    "2": "Standard",
+    "2": "ECO",
+    "3": "Power",
+    "4": "Automatic",
 }
 
 SENSOR_DETAILS = {
@@ -27,14 +68,14 @@ SENSOR_DETAILS = {
         "device_class": SensorDeviceClass.WATER,
         "state_class": "total_increasing"
     },
-    "raw_water_hardness": {
-        "name": "Raw water hardness",
+    "input_water_hardness": {
+        "name": "Input water hardness",
         "endpoint": "getIWH",
         "unit": "°dH",
         "scale": 1
     },
-    "target_water_hardness": {
-        "name": "Target water hardness",
+    "output_water_hardness": {
+        "name": "Output water hardness",
         "endpoint": "getOWH",
         "unit": "°dH",
         "scale": 1,
@@ -49,6 +90,12 @@ SENSOR_DETAILS = {
         "name": "Salt supply",
         "endpoint": "getSS1",
         "unit": "weeks",
+        "entity_category": EntityCategory.DIAGNOSTIC
+    },
+    "regeneration_mode": {
+        "name": "Regeneration mode",
+        "endpoint": "getRMO ",
+        "code_dict": REGEN_MODE_CODES,
         "entity_category": EntityCategory.DIAGNOSTIC
     },
     "regeneration_state": {
@@ -67,6 +114,12 @@ SENSOR_DETAILS = {
         "name": "Water until regeneration",
         "endpoint": "getRE1",
         "unit": "L",
+        "entity_category": EntityCategory.DIAGNOSTIC
+    },
+    "regeneration_schedule": {
+        "name": "Regeneration schedule",
+        "endpoint": "getRPD",
+        "unit": "days",
         "entity_category": EntityCategory.DIAGNOSTIC
     },
     "wifi_state": {
@@ -108,4 +161,20 @@ SERVICES = {
         "name": "Generic service call",
         "endpoint": "set/{endpoint}/{data}"
     },
+    "clear_alarms": {
+        "name": "Clear alarms",
+        "endpoint": "set/ala/255"
+    },
+    "set_regeneration_mode": {
+        "name": "Set regeneration mode",
+        "endpoint": "set/rmo/{mode}"
+    },
+    "set_regeneration_interval": {
+        "name": "Set regeneration interval",
+        "endpoint": "set/rpd/{period}"
+    },
+    "set_regeneration_time": {
+        "name": "Set regeneration time",
+        "endpoint": "set/rtm/{time}"
+    }
 }
